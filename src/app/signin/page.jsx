954bc9360c7d84b8710f781e6650a51c7c9b765f
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2025-06-16 17:38:57 +0300

    admin role to test

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -38,7 +38,8 @@ function page() {
         showToast('Login successful!', 'success');
         router.push('/');
         localStorage.setItem('Token', Token);
-        localStorage.setItem('Role', user.role);
+        // localStorage.setItem('Role', user.role);
+        localStorage.setItem('Role', Admin);
         localStorage.setItem('Id', user.user_id);
         localStorage.setItem('Name', user.first_name);
         console.log('Name', user.first_name);
